b6440b91f75b2f89218a04a674895ee7b41f06e8
rafaelsantos1993 <rafaellemos123@gmail.com>
2022-12-14 22:55:16 -0300

    Fixed addButton and cancelButton functions

--- bookHandler.js
+++ bookHandler.js
@@ -1,3 +1,5 @@
+//variables 
+
 const addButton = document.getElementById("add-button");
 const removeButton = document.getElementById("remove-button");
 const postDialogWindow = document.getElementById("post-book");
@@ -23,14 +25,18 @@ addButton.addEventListener("click", () => {
     postDialogWindow.showModal();
 });
 
+//close the dialog 
+cancelButton.addEventListener("click", ()=> {
+    postDialogWindow.close();
+})
+
 //gets information and creats a new book 
-postDialogWindow.addEventListener("close", () => {
+confirmButton.addEventListener("click", () => {
     //makes a new mook with the constructor 
     let newBook = new makeBook(author.value, title.value, page.value, read.checked)
     
     //add to the array of information
     myLibrary.push(newBook)
-
     //loops through the array, posting each new book in a post card 
     for (i=0; i<myLibrary.length; i++){
         let newPostCard = document.createElement("div");
@@ -40,9 +46,13 @@ postDialogWindow.addEventListener("close", () => {
         let pPage = document.createElement("p");
         let pRead = document.createElement("p");
         pTitle.textContent += myLibrary[i]["title"];
+        pTitle.classList.add('book-title');
         pAuthor.textContent += myLibrary[i]["author"];
+        pAuthor.classList.add('book-author');
         pPage.textContent += myLibrary[i]["page"];
+        pPage.classList.add('book-page');
         myLibrary[i]["read"] == true ? pRead.textContent += "Read" : pRead.textContent += "Not Read"
+        pRead.classList.add("book-page");
         newPostCard.appendChild(pTitle);
         newPostCard.appendChild(pAuthor);
         newPostCard.appendChild(pPage);
